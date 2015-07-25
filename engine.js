7eb2c6e7217a9fd0306eaf1ac6dfe0f34f15fab0
Monty Anderson <monty.linux@gmail.com>
2015-07-25 19:48:11 +0100

    fixed scope

--- engine.js
+++ engine.js
@@ -1,8 +1,11 @@
+var fs = require("fs"),
+    mustache = require("mustache");
+
 module.exports = function(filePath, options, callback) {
     fs.readFile(filePath, function(err, data) {
         if (err) return callback(new Error(err));
 
         var html = mustache.render(data.toString(), options);
         return callback(null, html);
-    })
+    });
 };
