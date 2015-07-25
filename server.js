4282d54fc6de8db68f0231e904d68f72bacb31a9
Monty Anderson <monty.linux@gmail.com>
2015-07-25 19:15:39 +0100

    now renders index.mustache

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var express = require("express"),
+var fs = require("fs"),
+    express = require("express"),
     mustache = require("mustache");
 
 var app = express();
 
 app.get("/", function(req, res) {
-    res.end("Hello, World!");
+    fs.readFile(__dirname + "/index.mustache", function(err, data) {
+        var html = mustache.render(data.toString(), {
+            name: "Jacob"
+        });
+
+        res.end(html);
+    });
 });
 
 app.listen(8080);
